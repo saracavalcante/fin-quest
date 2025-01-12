8623c9b2b4249ab1a0dec3964d890b85661f3f65
Sara Cavalcante <designersaracavalcante@gmail.com>
2025-01-12 15:49:00 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FinQuest"
+include(":app")
+include(":features")
+include(":features:home")
